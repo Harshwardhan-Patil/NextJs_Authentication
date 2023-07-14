bf879041db7462de8f42062f5c8a7e593a9043b5
Harshwardhan Patil <harshp3302@gmail.com>
2023-07-14 12:12:38 +0530

    add revalidate in /api/me route

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,20 +1,18 @@
-'use server'
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModels";
 import { connect } from "@/dbConfig/dbConfig";
 import mongoose from "mongoose";
-import { cookies } from 'next/headers';
-import jwt from 'jsonwebtoken';
+import { getDataFromToken } from "@/helper/getDataFromToken";
 
 
 connect();
 
+export const revalidate = 0;
+
 export async function GET(request: NextRequest) {
 
     try {
-        const token = cookies().get('token')?.value || '';
-        const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
-        const id = decodedToken.id;
+        const id = await getDataFromToken(request);
         if (mongoose.isValidObjectId(id)) {
             const user = await User.findById(id).select('-password');
             const response = NextResponse.json({
